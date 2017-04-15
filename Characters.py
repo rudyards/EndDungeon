58caf465f588187b08b716ed1640d0097e897f05
rdk25 <rkaner25@gmail.com>
2017-04-15 15:43:27 -0700

    corrected the calling list thingy
    corrected the calling list thingy

--- Characters.py
+++ Characters.py
@@ -8,12 +8,18 @@ class Character:
 class Merchant(Character):
     merchantListNumber = random.randint(1,3)
     if merchantListNumber = 1:
+        self.items = Item.merchantList1[:]
     elif merchantListNumber = 2:
+        self.items = Item.merchantList2[:]
     else:
+        self.items = Item.merchantList3[:]
 
 class Blacksmith(Character):
     blacksmithListNumber = random.randint(1,3)
     if blacksmithListNumber = 1:
+        self.items = Item.blacksmithList1[:]
     elif blacksmithListNumber = 2:
+        self.items = Item.blacksmithList2[:]
     else:
+        self.items = Item.blacksmithList3[:]
 
