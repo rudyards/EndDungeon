975c88bb182d9dd884c78930109e6f56ae519ec4
rdk25 <rkaner25@gmail.com>
2017-04-27 23:44:35 -0700

    rom descrips

--- maps.py
+++ maps.py
@@ -21,6 +21,10 @@ def roomdescriber():
             keyword = "an unshakeable feeling of dread"
         if secondroll == 3:
             keyword = "a permeating sense of unease"
+        if secondroll == 4:
+            keyword = "the souls of long-lost adventurers"
+        if secondroll == 5:
+            keyword = "ominous pools of ink (or is it ink?)"
         description = "This room is filled with "+keyword
 
 
@@ -32,6 +36,10 @@ def roomdescriber():
             keyword = "used as storage, but it has since been looted"
         if secondroll == 3:
             keyword = "the site of a gory battle, and faint blood stains remain"
+        if secondroll == 4:
+            keyword = "well-furnished, and a dilapidated armchair remains"
+        if secondroll == 5:
+            keyword = "once once used for torturing prisoners"
         description = "This room was once "+keyword
 
 
@@ -43,17 +51,25 @@ def roomdescriber():
             keyword = "and there are concerning scratchmarks on the ceiling"
         if secondroll == 3:
             keyword = "and is that a pair of eyes peering out at you?"
+        if secondroll == 4:
+            keyword = "and there are mysterious symbols on the walls"
+        if secondroll == 5:
+            keyword = "and scorched skulls litter the floor"
         description = "Thick cobwebs clutter the corners of the room "+keyword
 
     else:
         
         if secondroll == 1:
-            keyword = "ancient things and mildew"
+            keyword = "of ancient things and mildew"
         if secondroll == 2:
-            keyword = "the unmistakable iron scent of blood"
+            keyword = "of the unmistakable iron scent of blood"
         if secondroll == 3:
             keyword = "fear"
-        description = "The room smells of "+keyword
+        if secondroll == 4:
+            keyword = "faintly of murder"
+        if seconroll == 5:
+            keyword = "inexplicably of cinnamon"
+        description = "The room smells "+keyword
 
     return description
 
