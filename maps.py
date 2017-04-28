36e687a0c1294bd75b6f248861b394e71b1c0137
rdk25 <rkaner25@gmail.com>
2017-04-27 23:30:57 -0700

    room descrips

--- maps.py
+++ maps.py
@@ -19,16 +19,20 @@ def roomdescriber():
             keyword = "a thin mist that covers the ground"
         if secondroll == 2:
             keyword = "an unshakeable feeling of dread"
+        if secondroll == 3:
+            keyword = "a permeating sense of unease"
         description = "This room is filled with "+keyword
 
 
     elif roll == 2:
         
         if secondroll == 1:
-            keyword = "was slept in, judging by the dust covered cots"
+            keyword = "slept in, judging by the dust covered cots"
         if secondroll == 2:
-            keyword = "was used as storage, but it has since been looted"
-        description = "This room once "+keyword
+            keyword = "used as storage, but it has since been looted"
+        if secondroll == 3:
+            keyword = "the site of a gory battle, and faint blood stains remain"
+        description = "This room was once "+keyword
 
 
     elif roll == 3:
@@ -37,6 +41,8 @@ def roomdescriber():
             keyword = "and the floor has a strange red mark on it"
         if secondroll == 2:
             keyword = "and there are concerning scratchmarks on the ceiling"
+        if secondroll == 3:
+            keyword = "and is that a pair of eyes peering out at you?"
         description = "Thick cobwebs clutter the corners of the room "+keyword
 
     else:
@@ -45,6 +51,8 @@ def roomdescriber():
             keyword = "ancient things and mildew"
         if secondroll == 2:
             keyword = "the unmistakable iron scent of blood"
+        if secondroll == 3:
+            keyword = "fear"
         description = "The room smells of "+keyword
 
     return description
